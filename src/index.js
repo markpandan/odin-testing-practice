440d5126e7fca744ab535b2e921b877263349198
markpandan <72681401+markpandan@users.noreply.github.com>
2025-05-05 23:42:24 +0800

    Implement capitalize, reverseString, and calculator functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-function sum(a, b) {
-  return a + b;
+export const capitalize = (string) => {
+  const words = string.split(" ");
+
+  let newString = "";
+  for (const word of words)
+    newString += `${word[0].toUpperCase() + word.substring(1)} `;
+
+  return newString.trimEnd();
+};
+
+export const reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
+
+export function Calculator(a, b) {
+  this.add = () => {
+    return a + b;
+  };
+  this.subtract = () => {
+    return a - b;
+  };
+  this.divide = () => {
+    return a / b;
+  };
+  this.multiply = () => {
+    return a * b;
+  };
 }
-module.exports = sum;
+
+export const caesarCipher = (string, shiftFactor) => {};
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,41 @@
-const sum = require("./index");
+import { capitalize, reverseString, Calculator, caesarCipher } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("capitalize first letter", () => {
+  expect(capitalize("odin")).toBe("Odin");
+});
+
+test("capitalize first letter", () => {
+  expect(capitalize("odin project")).toBe("Odin Project");
+});
+
+test("reverses string correctly", () => {
+  expect(reverseString("odin")).toBe("nido");
+});
+
+test("reverses string correctly with space", () => {
+  expect(reverseString("odin project")).toBe("tcejorp nido");
+});
+
+test("can calculate add operations", () => {
+  const calculate = new Calculator(3, 3);
+  expect(calculate.add()).toBe(6);
+});
+
+test("can calculate subtract operations", () => {
+  const calculate = new Calculator(3, 3);
+  expect(calculate.subtract()).toBe(0);
+});
+
+test("can calculate multiply operations", () => {
+  const calculate = new Calculator(3, 3);
+  expect(calculate.multiply()).toBe(9);
+});
+
+test("can calculate divide operations", () => {
+  const calculate = new Calculator(3, 3);
+  expect(calculate.divide()).toBe(1);
+});
+
+test("can calculate divide operations", () => {
+  expect(calculate.divide()).toBe(1);
 });
