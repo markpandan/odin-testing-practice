ed44c25cca60b922daefade2a89fc33b41e7dbbf
markpandan <72681401+markpandan@users.noreply.github.com>
2025-05-06 00:33:54 +0800

    Change the test descriptions

--- src/index.test.js
+++ src/index.test.js
@@ -6,55 +6,55 @@ import {
   analyzeArray,
 } from "./index";
 
-test("capitalize first letter", () => {
+test("capitalize function can capitalize first letter", () => {
   expect(capitalize("odin")).toBe("Odin");
 });
 
-test("capitalize first letter", () => {
+test("capitalize function can capitalize first letter for every words", () => {
   expect(capitalize("odin project")).toBe("Odin Project");
 });
 
-test("reverses string correctly", () => {
+test("reverseString function can reverse a string correctly", () => {
   expect(reverseString("odin")).toBe("nido");
 });
 
-test("reverses string correctly with space", () => {
+test("reverseString function can reverse a string correctly with space", () => {
   expect(reverseString("odin project")).toBe("tcejorp nido");
 });
 
-test("can calculate add operations", () => {
+test("Calculator object can calculate add operations", () => {
   const calculate = new Calculator(3, 3);
   expect(calculate.add()).toBe(6);
 });
 
-test("can calculate subtract operations", () => {
+test("Calculator object can calculate subtract operations", () => {
   const calculate = new Calculator(3, 3);
   expect(calculate.subtract()).toBe(0);
 });
 
-test("can calculate multiply operations", () => {
+test("Calculator object can calculate multiply operations", () => {
   const calculate = new Calculator(3, 3);
   expect(calculate.multiply()).toBe(9);
 });
 
-test("can calculate divide operations", () => {
+test("Calculator object can calculate divide operations", () => {
   const calculate = new Calculator(3, 3);
   expect(calculate.divide()).toBe(1);
 });
 
-test("can shift words", () => {
+test("caesarCipher function can shift words", () => {
   expect(caesarCipher("xyz", 3)).toBe("abc");
 });
 
-test("can shift words while maintaining cases", () => {
+test("caesarCipher function can shift words while maintaining cases", () => {
   expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
 });
 
-test("can shift words but exceptionally avoids punctuations", () => {
+test("caesarCipher function can shift words but exceptionally avoids punctuations", () => {
   expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
 
-test("analyzeArray can output an object with the correct average, min, max, and length value", () => {
+test("analyzeArray function can output an object with the correct average, min, max, and length value", () => {
   const object = analyzeArray([1, 8, 3, 4, 2, 6]);
   expect(object.average).toBe(4);
   expect(object.min).toBe(1);
